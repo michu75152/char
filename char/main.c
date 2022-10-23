36c8d054c50bd34da1e99c43f01a7af3dcf0d1fb
michu75152 <92825640+michu75152@users.noreply.github.com>
2022-10-23 14:00:57 +0200

    Initial Commit

---
+++ char/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  char ss
+//
+//  Created by Michał Dżugaj on 23/10/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, Woxaxaxrld!\n");
+    return 0;
+}
